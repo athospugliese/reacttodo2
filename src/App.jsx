669acb3bb3e42dbb2a1d8707618fc5926e01220f
aluno <aluno0@unicap.br>
2022-09-14 17:27:20 -0300

    study 01

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const App = () => { 
+
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const [ value, setValue ] = useState('');
+
+  const erase = () => {
+    setValue=('');
+  }
+  
+  const submit = () => {
+    console.log('submit', value);
+      erase();
+  }
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  }
+
+  const onKeyDown = event => {
+    if (event.which === ENTER_KEY) {
+        submit();
+    } else if 
+      (event.which === ESCAPE_KEY) {
+        erase();
+      }
+  };
+
+  return (
+
+  <section id='app' className='container'>
+    <header>
+        <h1 className='title'>todo</h1>
+    </header>
+    <section className='main'>
+      <input className='new-todo'
+      placeholder='O que precisa ser feito?'
+      value={value}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      />
+    </section>
+  </section>
+);
+  }
+
+export default App
